142d390d940241f7d4bea51e3256c6840b857296
Aashu <Aashupatil218@gmail.com>
2025-11-07 22:46:00 +0530

    data types

---
+++ DataTypes.java
@@ -0,0 +1,40 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        
+        int myNum = 5;               // Integer (whole number)
+        short myShort = 5000;      // Short integer
+        long myLong = 15000000000L; // Long integer
+        byte myByte = 100;        // Byte integer
+
+
+        float myFloatNum = 5.99f;    // Floating point number
+        double myDouble = 19.99;   // Double precision floating point number
+
+        char myLetter = 'D';         // Character
+        String myText = "Hello";     // String
+
+        boolean myBool = true;       // Boolean
+
+
+        System.out.println("Integer: " + myNum);
+        System.out.println("Short: " + myShort);
+        System.out.println("Long: " + myLong);
+        System.out.println("Byte: " + myByte);
+
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Double: " + myDouble);
+
+        System.out.println("Character: " + myLetter);
+        System.out.println("String: " + myText);
+
+        System.out.println("Boolean: " + myBool);
+
+        
+
+
+
+        
+    }
+    
+}
