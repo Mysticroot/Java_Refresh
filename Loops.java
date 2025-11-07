c45bd387a8aa26601d0494fbbd23a6697d882835
Aashu <Aashupatil218@gmail.com>
2025-11-07 22:59:00 +0530

    loops

---
+++ Loops.java
@@ -0,0 +1,28 @@
+public class Loops {
+    
+    public static void main(String[] args) {
+        
+        // For Loop
+        System.out.println("For Loop:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+
+        // While Loop
+        System.out.println("\nWhile Loop:");
+        int j = 0;
+        while (j < 5) {
+            System.out.println("Iteration: " + j);
+            j++;
+        }
+
+        // Do-While Loop
+        System.out.println("\nDo-While Loop:");
+        int k = 0;
+        do {
+            System.out.println("Iteration: " + k);
+            k++;
+        } while (k < 5);
+
+    }
+}
