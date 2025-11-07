3f66c383aa863836a712aae5898544703beb23e7
Aashu <Aashupatil218@gmail.com>
2025-11-07 23:10:09 +0530

    simple class

---
+++ JavaClass.java
@@ -0,0 +1,22 @@
+
+
+class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+}
+
+public class JavaClass {
+
+    public static void main(String[] args) {
+        
+
+        Calculator calc = new Calculator();
+        
+        int sum = calc.add(5, 10);
+        System.out.println("Sum: " + sum);
+    }
+    
+}
