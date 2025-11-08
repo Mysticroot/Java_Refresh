0fab0a02577fd3cd214a466cb3bbd633bc3ce19f
Aashu <Aashupatil218@gmail.com>
2025-11-08 10:25:40 +0530

    2d array

--- Array.java
+++ Array.java
@@ -37,5 +37,45 @@ public class Array {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Element at index " + i + ": " + numbers[i]);
         }
+
+
+
+
+
+
+        //2-D Array
+        System.out.println("\n-- 2-D Array example --");
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //another way to declare and initialize 2-D array
+        int[][] matrix1 = new int[3][3];
+        matrix1[0][0] = 1;
+        matrix1[0][1] = 2;
+        matrix1[0][2] = 3;
+        matrix1[1][0] = 4;
+        matrix1[1][1] = 5;
+        matrix1[1][2] = 6;
+        matrix1[2][0] = 7;
+        matrix1[2][1] = 8;
+        matrix1[2][2] = 9;
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
